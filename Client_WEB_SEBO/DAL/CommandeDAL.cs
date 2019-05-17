cceae90a6e780d79b6e9ee1e1062beb3aa2afdc4
Maf38 <mafal.gai@outlook.com>
2019-05-17 11:49:07 +0200

    Bon week end!

--- Client_WEB_SEBO/DAL/CommandeDAL.cs
+++ Client_WEB_SEBO/DAL/CommandeDAL.cs
@@ -51,5 +51,54 @@ namespace Client_WEB_SEBO.DAL
                 }
                 return commandes;
             }
+
+/*
+        public static commande GetCommande(string idCommande)
+        {
+            commande commCherche = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BASE_ADRESS);
+
+                // Called article default GET All records
+                //GetAsync to send a GET request   
+                // PutAsync to send a PUT request
+
+                string uriREST = "articles/" + idCommande;
+
+                var responseTask = client.GetAsync(uriREST);
+                responseTask.Wait();
+
+                //To store result of web api response.   
+                var result = responseTask.Result;
+
+                //If success received   
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<Article>();
+                    readTask.Wait();
+
+                    article = readTask.Result;
+                }
+                else
+                {
+                    //Error response received   
+                    article = new Article();
+                    article.auteur = "na";
+                    article.description = "na";
+                    article.editeur = "na";
+                    article.genre = "na";
+                    article.idCommande = 0;
+                    article.image = "na";
+                    article.prix = 0;
+
+                    //ModelState.AddModelError(string.Empty, "Server error try after some time.");
+                }
+
+
+            }
+            return article;
+        }*/
     }
 }
